04948b414d3d0c8de19578454d802bfd336810b5
Taza67 <tazaakil67@gmail.com>
2022-05-15 09:40:00 +0200

    fix: compter les occurences des fichiers des sous-dossiers
    - parcourir récursivement les dossiers lors du calcul des occurences
    - éviter un arbre vide quand un dossier est compressé

--- src/en_tete.c
+++ src/en_tete.c
@@ -53,7 +53,20 @@ void ecrire_entete(file* liste_fichiers, FILE* archive, int nombre_fichiers, noe
 void calculer_occurences_totales(file* liste_fichiers, int nombre_fichiers, int tab_occurence[256]) {
     int i = 0;
     for (i = 0; i < nombre_fichiers; i++) {
-        if (liste_fichiers[i].type == 'f')
+        if (liste_fichiers[i].type == 'f') {
             occurence(liste_fichiers[i].nom, tab_occurence);
+        } else if (liste_fichiers[i].type == 'd') {
+            int nombre_sous_fichiers = renvoyer_nombre_fichiers_dossier(liste_fichiers[i].nom);
+            file * sous_fichiers = lister_fichiers_dossier(liste_fichiers[i].nom);
+            int j = 0;
+            for (j = 0; j < nombre_sous_fichiers; j++) {
+                char * ancien_nom = sous_fichiers[j].nom;
+                sous_fichiers[j].nom = creer_chemin_fichier(liste_fichiers[i].nom, ancien_nom);
+                libere(ancien_nom);
+            }
+            calculer_occurences_totales(sous_fichiers, nombre_sous_fichiers, tab_occurence);
+            for (j = 0; j < nombre_sous_fichiers; j++) libere(sous_fichiers[j].nom);
+            libere(sous_fichiers);
+        }
     }
 }
